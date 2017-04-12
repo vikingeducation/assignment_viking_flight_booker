f79c3482900225a7cae33fabc88206859763526d
Egle Libby <eglelibby@gmail.com>
2017-04-12 16:46:45 -0400

    moving forward

--- warmups.sql
+++ warmups.sql
@@ -14,6 +14,26 @@ Get a list of all airports in Kadeton
     WHERE cities.name = 'Kadeton';
 
 Get a list of all payment methods used on itineraries by the user with email address 'senger.krystel@marvin.io'
+
+  SELECT DISTINCT payment_method as "Payment method"
+  FROM users
+  JOIN itineraries ON users.id = itineraries.user_id
+  WHERE email='senger.krystel@marvin.io';
+
 Get a list of prices of all flights whose origins are in Kochfurt Probably International Airport.
+
+  SELECT flights.price as "Prices"
+  FROM flights
+  JOIN airports ON flights.origin_id = airports.id
+  WHERE long_name = 'Kochfurt Probably International Airport';
+
 Find a list of all Airport names and codes which connect to the airport coded LYT.
+
+  SELECT destination_id, code
+  FROM airports
+  JOIN flights ON airports.id = flights.origin_id;
+  WHERE code = 'LYT';
+
+
+
 Get a list of all airports visited by user Krystel Senger after January 1, 2012. (Hint, see if you can get a list of all ticket IDs first).
