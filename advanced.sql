9f4e8d8fd104a13d21950d9030559405e54d2350
Christian Florez <florez.christian@gmail.com>
2017-05-31 14:07:51 -0400

    finish aggregation

---
+++ advanced.sql
@@ -0,0 +1,12 @@
+-- 1. Find the most popular travel destination for users who live in California.
+
+-- 2. How many flights have round trips possible? In other words, we want the count of all airports 
+--    where there exists a flight FROM that airport and a later flight TO that airport.
+
+-- 3. Find the cheapest flight that was taken by a user who only had one itinerary.
+
+-- 4. Find the average cost of a flight itinerary for users in each state in 2012.
+
+-- 5. Bonus: You're a tourist. It's May 6, 2013. Book the cheapest set of flights over the next six weeks 
+-- that connect Oregon, Pennsylvania and Arkansas, but do not take any flights over 400 miles in distance. 
+-- Note: This can be ~50 lines long but doesn't require any subqueries.
