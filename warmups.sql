a9ef212521b2b7a183539bcc1461d0e761739198
Greg Filipczak <gregfilipczak@gmail.com>
2017-08-02 20:50:44 +0000

    3 done

--- warmups.sql
+++ warmups.sql
@@ -1,19 +1,25 @@
 -- airports, users, cities, states, flights,
 
 
-1.
+-- 1. Get a list of all users in California
 SELECT *
 FROM users
-JOIN states ON ()
+JOIN states ON (users.state_id = states.id)
+WHERE states.name = 'California';
 
-2.
+-- 2.Get a list of all airports in Kadeton
 
-SELECT *
-FROM cities
-WHERE name = 'Hoegerberg';
 
 SELECT *
 FROM airports
-WHERE city_id = 872;
+JOIN cities ON (cities.id = airports.city_id)
+WHERE name = 'Kadenview';
 
 3.
+
+-- hansen.lawson@marvin.io
+
+SELECT payment_method
+FROM itineraries
+JOIN users ON (itineraries.user_id = users.id)
+WHERE users.email = 'hansen.lawson@marvin.io';
