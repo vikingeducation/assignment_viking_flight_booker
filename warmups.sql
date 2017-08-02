1bde91d54c85d260404a2a12ff04f0fa341da20a
Kara Thrash <dvdonato@gmail.com>
2017-08-02 18:32:06 -0400

    cruising airlines usaq

--- warmups.sql
+++ warmups.sql
@@ -40,6 +40,39 @@ JOIN itineraries on (tickets.itinerary_id = itineraries.id)
 JOIN users on (itineraries.user_id = users.id)
 WHERE users.email = 'hansen.lawson@marvin.io' AND flights.departure_time >= '2012-01-01 16:10:28.273072';
 
-SELECT flight_id
-FROM flights
-WHERE flights.destination_id = 'california'
+SELECT flights.id,
+flights.price
+FROM flights JOIN airports ON (flights.destination_id = airports.id)
+JOIN states ON (states.id = airports.state_id)
+WHERE states.name = 'California'
+ORDER BY flights.price DESC
+limit 5
+
+SELECT flights.id,
+flights.distance
+FROM flights JOIN tickets ON (flights.id = tickets.flight_id)
+JOIN itineraries ON (itineraries.id = tickets.itinerary_id)
+JOIN users ON (users.id = itineraries.user_id)
+WHERE users.email = 'hansen.lawson@marvin.io'
+ORDER BY flights.distance DESC
+limit 1
+
+SELECT AVG(flights.distance)
+FROM flights JOIN airports ON (flights.origin_id = airports.id)
+JOIN states ON (states.id = airports.state_id)
+WHERE states.name = 'California'
+
+SELECT users.username,
+SUM(flights.price) as totalPrice
+FROM flights JOIN tickets ON (flights.id = tickets.flight_id)
+JOIN itineraries ON (itineraries.id = tickets.itinerary_id)
+JOIN users ON (users.id = itineraries.user_id)
+GROUP BY users.username
+ORDER BY totalPrice DESC
+limit 5
+
+SELECT COUNT(flights.id)
+FROM flights JOIN airports ON (flights.destination_id = airports.id)
+JOIN states ON (airports.state_id = state.id) AS DestState
+JOIN airports ON (flights.origin_id = airports.id)
+WHERE DestState.state_id != 'Delaware'
