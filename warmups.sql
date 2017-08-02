f66e86e0c4acfcdc924c4fe30b1d094ef1ceaaff
Kara Thrash <dvdonato@gmail.com>
2017-08-02 17:18:11 -0400

    more sql searches

--- warmups.sql
+++ warmups.sql
@@ -19,7 +19,16 @@ WHERE name = 'Kadenview';
 
 -- hansen.lawson@marvin.io
 
-SELECT payment_method
-FROM itineraries
-JOIN users ON (itineraries.user_id = users.id)
+SELECT payment_method;
+FROM itineraries;
+JOIN users ON (itineraries.user_id = users.id);
 WHERE users.email = 'hansen.lawson@marvin.io';
+
+SELECT price
+FROM flights JOIN airports ON (flights.origin_id = airports.city_id)
+WHERE airports.long_name = 'Kochview Probably International Airport'
+
+SELECT airports.long_name,
+airports.code
+FROM airports JOIN flights ON (flights.origin_id = airports.id)
+WHERE flights.destination_id = 'LYT'
