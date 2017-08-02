b1b43f4b8b25c40fd351f6a909f577a8f9ecd26b
Alex-Willenbrink <awillenbrink@gmail.com>
2017-08-02 18:03:10 -0500

    started advanced

--- aggregation.sql
+++ aggregation.sql
@@ -32,3 +32,24 @@ JOIN tickets ON (itineraries.id = tickets.itinerary_id)
 JOIN flights ON (tickets.flight_id = flights.id)
 WHERE flights.arrival_time BETWEEN '2013-01-01'::date AND '2013-12-12 23:59:59'::date
 GROUP BY name ORDER BY total DESC LIMIT 3;
+
+-- 5. Count all flights to OR from the city of Smithshire that did not land in Delaware
+SELECT COUNT(*) as flights_total
+FROM flights JOIN airports
+ON ((flights.origin_id = airports.id) OR (flights.destination_id = airports.id))
+JOIN cities ON (airports.city_id = cities.id) JOIN states ON (states.id = airports.state_id)
+WHERE states.name <> 'Delaware' AND cities.name = 'Port Jennings';
+
+-- 6. Return the range of lengths of flights in the system(the maximum, and the minimum).
+SELECT MAX(arrival_time - departure_time) AS max_duration,
+  MIN(arrival_time - departure_time)
+  AS min_duration FROM flights;
+
+-- Queries Advanced
+-- 1. Find the most popular travel destination for users who live in California.
+SELECT * FROM flights JOIN tickets ON (flights.id = tickets.flight_id)
+  JOIN itinerary ON (tickets.itinerary_id = itinerary.id)
+  JOIN users ON (users.id = itinerary.user_id)
+  WHERE user_id IN (SELECT users.id FROM users
+  JOIN states ON (users.state_id = states.id)
+  WHERE states.name = 'California';
