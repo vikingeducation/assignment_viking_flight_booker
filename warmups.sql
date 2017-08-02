92828892a427f7ef9fc6ef4f8059fad784a04809
Greg Filipczak <gregfilipczak@gmail.com>
2017-08-02 20:37:49 +0000

    added wrong queries

---
+++ warmups.sql
@@ -0,0 +1,19 @@
+-- airports, users, cities, states, flights,
+
+
+1.
+SELECT *
+FROM users
+JOIN states ON ()
+
+2.
+
+SELECT *
+FROM cities
+WHERE name = 'Hoegerberg';
+
+SELECT *
+FROM airports
+WHERE city_id = 872;
+
+3.
