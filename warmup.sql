6e5cdb904952e4a16eb1107d8046715903a8ae2c
Eric Glover <etgt28@mail.missouri.edu>
2017-08-02 16:57:32 -0500

    brain la;ksdjfl;aksdjf

--- warmup.sql
+++ warmup.sql
@@ -11,12 +11,12 @@ WHERE cities.name = 'Kadeton'
 
 SELECT *
 FROM airports JOIN cities ON cities.id = airports.city_id
-WHERE cities.name = 'Kadeton'
+WHERE cities.name = 'New Jeffrey'
 
 --there is no city called Kadeton
---SELECT *
---FROM cities
---WHERE name ILIKE 'k%'
+SELECT *
+FROM cities
+WHERE name ILIKE 'k%'
 
 --Get a list of all payment methods used on itineraries by the user with email address 'senger.krystel@marvin.io'
 --no one with that email??
@@ -45,13 +45,54 @@ WHERE airports.long_name = 'Brooksfurt Probably International Airport';
 
 
 --Find a list of all Airport names and codes which connect to the airport coded LYT
-SELECT airports.long_name, code
+SELECT *
 FROM airports
-WHERE code LIKE 'LYT'
-LIMIT 5
+WHERE code = 'NDL';
+
+SELECT long_name, code
+FROM flights
+JOIN airports AS destination ON flights.destination_id = destination.id
+WHERE destination.code = 'NDL';
 
 
+JOIN airports source ON flight.origin_id = source.id
+--Get a list of all airports visited by user Krystel Senger after January 1, 2012. (Hint, see if you can get a list of all ticket IDs first).
+
+CREATE TABLE tickets (
+    id integer NOT NULL,
+    itinerary_id integer,
+    flight_id integer,
+    created_at timestamp without time zone,
+    updated_at timestamp without time zone
+);
+SELECT * FROM tickets JOIN flights LIMIT 10
+
+--use ticket to grab flight to grab user_id
+SELECT *
+FROM users
+JOIN tickets ON tickets.user_id = id
+JOIN itineraries ON itineraries.user_id = tickets.itinerary_id
+WHERE users.first_name = 'Krystel'
+AND users.last_name = 'Senger'
+
+SELECT *
+FROM tickets
+JOIN itineraries ON itineraries.id = tickets.itinerary_id
+JOIN users ON itineraries.user_id = users.id
+WHERE users.first_name = 'Krystel'
+AND users.last_name = 'Senger'
+
+
+
+SELECT * FROM tickets JOIN itineraries ON itineraries.user_id = tickets.itinerary_id LIMIT 10
+SELECT * FROM tickets
+SELECT * FROM flights
+SELECT * FROM users
+SELECT * FROM itineraries
 --SCRAP CODE
+
+SELECT * FROM users
+WHERE username ILIKE '%kry%';
 SELECT price, airports.long_name
 FROM airports JOIN flights on flights.origin_id = airports.id
 WHERE airports.long_name = 'Brooksfurt Probably International Airport';
