0962919e891353a58e671f98ffeb0e278152fae1
GeneTinderholm <tinderholmgene@gmail.com>
2017-11-30 13:57:39 -0600

    do actual assignment

--- 2_aggregation.sql
+++ 2_aggregation.sql
@@ -36,7 +36,7 @@ limit 3;
 
 -- Count all flights to OR from the city of Smithshire that did not land in Delaware
 
-SELECT * 
+SELECT COUNT(flights.id) 
 FROM flights 
 JOIN airports ON (origin_id = airports.id or destination_id = airports.id) 
 JOIN cities ON (cities.id = airports.city_id) 
@@ -47,4 +47,5 @@ WHERE cities.name = 'Smithhaven' and states.name != 'Delaware';
 
 -- Return the range of lengths of flights in the system(the maximum, and the minimum).
 
-SELECT min(arrival_time - departure_time), max(arrival_time - departure_time) FROM flights;
+SELECT min(arrival_time - departure_time), max(arrival_time - departure_time) 
+FROM flights;
