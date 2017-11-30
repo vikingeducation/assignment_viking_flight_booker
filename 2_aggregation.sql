d8f2a7d7e73540a60e5112838d6ba366d1a93d0c
GeneTinderholm <tinderholmgene@gmail.com>
2017-11-30 14:46:52 -0600

    demo changes

--- 2_aggregation.sql
+++ 2_aggregation.sql
@@ -2,20 +2,21 @@
 
 -- Find the top 5 most expensive flights that end in California.
 
-SELECT * 
-FROM flights 
-JOIN states ON(states.id  = destination_id) 
-WHERE states.name = 'California' 
-ORDER BY price DESC 
+SELECT *
+FROM flights
+JOIN airports ON(destination_id = airports.id)
+JOIN states ON(airports.state_id  = states.id)
+WHERE states.name = 'California'
+ORDER BY price DESC
 LIMIT 5;
 
 -- Find the shortest flight that username 'emelia' took.
 
-SELECT * 
-FROM flights 
-JOIN tickets ON flights.id=tickets.flight_id 
-WHERE itinerary_id = 291 
-ORDER BY (flights.arrival_time - flights.departure_time) 
+SELECT *
+FROM flights
+JOIN tickets ON flights.id=tickets.flight_id
+WHERE itinerary_id = 291
+ORDER BY (flights.arrival_time - flights.departure_time)
 LIMIT 1;
 
 -- Find the average flight distance for every city in California
@@ -31,15 +32,15 @@ GROUP BY cities.name;
 
 --users.id -> itinearies.id -> tickets.flight_id -> flights.price
 /*
-SELECT username, price 
-FROM users 
-JOIN itineraries ON (users.id = itineraries.id) 
-JOIN tickets ON (users.itinerary_id = tickets.itinerary_id) 
-JOIN flights ON (tickets.flight_id = flights.id) 
-GROUP BY users.username  
-ORDER BY sum(price) DESC  
+SELECT username, price
+FROM users
+JOIN itineraries ON (users.id = itineraries.id)
+JOIN tickets ON (users.itinerary_id = tickets.itinerary_id)
+JOIN flights ON (tickets.flight_id = flights.id)
+GROUP BY users.username
+ORDER BY sum(price) DESC
 LIMIT 3;
-*/
+ */
 
 SELECT users.username, sum(price)
 FROM flights JOIN tickets ON(flights.id = tickets.flight_id)
@@ -51,16 +52,16 @@ limit 3;
 
 -- Count all flights to OR from the city of Smithshire that did not land in Delaware
 
-SELECT COUNT(flights.id) 
-FROM flights 
-JOIN airports ON (origin_id = airports.id or destination_id = airports.id) 
-JOIN cities ON (cities.id = airports.city_id) 
-JOIN states ON(states.id = airports.state_id) 
+SELECT COUNT(flights.id)
+FROM flights
+JOIN airports ON (origin_id = airports.id or destination_id = airports.id)
+JOIN cities ON (cities.id = airports.city_id)
+JOIN states ON(states.id = airports.state_id)
 WHERE cities.name = 'Smithhaven' and states.name != 'Delaware';
 
 --city_id -> airpots.city_id (to get airports) -> flights (connecting origin & destination)
 
 -- Return the range of lengths of flights in the system(the maximum, and the minimum).
 
-SELECT min(arrival_time - departure_time), max(arrival_time - departure_time) 
+SELECT min(arrival_time - departure_time), max(arrival_time - departure_time)
 FROM flights;
