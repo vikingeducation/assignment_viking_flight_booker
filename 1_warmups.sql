a4e021d3f44be1390fc11d477d7c1d9196067f30
GeneTinderholm <tinderholmgene@gmail.com>
2017-11-30 13:12:29 -0600

    solve and format

--- 1_warmups.sql
+++ 1_warmups.sql
@@ -6,21 +6,21 @@ WHERE state_id=215
 
 -- Get a list of all airports in Gwendolynfort
 
-select *
-from airports
-where city_id=863;
+SELECT *
+FROM airports
+WHERE city_id=863;
 
 -- Get a list of all payment methods used on itineraries by the user with email address 'kallie.weimann@dach.biz'
 
-select *
-from itineraries
-where user_id=429;
+SELECT *
+FROM itineraries
+WHERE user_id=429;
 
 -- Get a list of prices of all flights whose origins are in Port Gerardbury Probably International Airport.
 
-select *
-from flights
-where origin_id=1
+SELECT *
+FROM flights
+WHERE origin_id=1
 
 -- Find a list of all Airport names and codes which connect to the airport coded ZAM.
 
@@ -28,4 +28,4 @@ SELECT * FROM airports WHERE code='ZAM';
 
 -- Get a list of all airports visited by user Sophie McGlynn after January 1, 2012. (Hint, see if you can get a list of all ticket IDs first).
 
-select long_name from flights JOIN tickets ON flights.id=tickets.flight_id JOIN airports ON (origin_id = airports.id  OR destination_id = airports.id) where itinerary_id = 291;
+SELECT long_name FROM flights JOIN tickets ON flights.id=tickets.flight_id JOIN airports ON (origin_id = airports.id  OR destination_id = airports.id) WHERE itinerary_id = 291;
--- 2_aggregation.sql
+++ 2_aggregation.sql
@@ -2,44 +2,49 @@
 
 -- Find the top 5 most expensive flights that end in California.
 
-select * from flights join states ON(states.id  = destination_id) where states.name = 'California' order by price DESC limit 5;
+SELECT * FROM flights JOIN states ON(states.id  = destination_id) WHERE states.name = 'California' ORDER BY price DESC limit 5;
 
 -- Find the shortest flight that username 'emelia' took.
 
-select * from flights JOIN tickets ON flights.id=tickets.flight_id where itinerary_id = 291 order by (flights.arrival_time - flights.departure_time) limit 1;
+SELECT * FROM flights JOIN tickets ON flights.id=tickets.flight_id WHERE itinerary_id = 291 ORDER BY (flights.arrival_time - flights.departure_time) limit 1;
 
 -- Find the average flight distance for every city in California
 
-select * from airports join states on (state_id = states.id) where states.name = 'California';
+SELECT * FROM airports JOIN states ON (state_id = states.id) WHERE states.name = 'California';
 
-select cities.name, AVG(distance)
-from airports join states on (states.id  = state_id)
-join flights on (flights.destination_id = airports.id  OR flights.origin_id = airports.id)
-join cities ON (cities.id  = city_id)
-where states.name  = 'California'
-group by cities.name;
+SELECT cities.name, AVG(distance)
+FROM airports JOIN states ON (states.id  = state_id)
+JOIN flights ON (flights.destination_id = airports.id  OR flights.origin_id = airports.id)
+JOIN cities ON (cities.id  = city_id)
+WHERE states.name  = 'California'
+GROUP BY cities.name;
 
 -- Find the 3 users who spent the most money on flights in 2013
 
-users.id -> itinearies.id -> tickets.flight_id -> flights.price
+--users.id -> itinearies.id -> tickets.flight_id -> flights.price
 
-SELECT username, price FROM users JOIN itineraries ON (users.id = itineraries.id) JOIN tickets on (users.itinerary_id = tickets.itinerary_id) JOIN flights on (tickets.flight_id = flights.id) group by users.username  order by sum(price) desc  limit 3;
+SELECT username, price FROM users JOIN itineraries ON (users.id = itineraries.id) JOIN tickets ON (users.itinerary_id = tickets.itinerary_id) JOIN flights ON (tickets.flight_id = flights.id) GROUP BY users.username  ORDER BY sum(price) DESC  limit 3;
 
-select users.username, sum(price)
-from flights join tickets ON(flights.id = tickets.flight_id)
-join itineraries on(itineraries.id = tickets.itinerary_id)
-join users ON (users.id = itineraries.user_id)
-group by users.username
-order by sum(price) desc
+SELECT users.username, sum(price)
+FROM flights JOIN tickets ON(flights.id = tickets.flight_id)
+JOIN itineraries ON(itineraries.id = tickets.itinerary_id)
+JOIN users ON (users.id = itineraries.user_id)
+GROUP BY users.username
+ORDER BY sum(price) DESC
 limit 3;
- select * from cities where name='Smithshire';
+ SELECT * FROM cities WHERE name='Smithshire';
 
 -- Count all flights to OR from the city of Smithshire that did not land in Delaware
 
-select * from cities join airports on (cities.id = airports.id) join flights on ()
+SELECT * 
+FROM flights 
+JOIN airports ON (origin_id = airports.id or destination_id = airports.id) 
+JOIN cities ON (cities.id = airports.city_id) 
+JOIN states ON(states.id = airports.state_id) 
+WHERE cities.name = 'Smithhaven' and states.name != 'Delaware';
 
-city_id -> airpots.city_id (to get airports) -> flights (connecting origin & destination)
+--city_id -> airpots.city_id (to get airports) -> flights (connecting origin & destination)
 
 -- Return the range of lengths of flights in the system(the maximum, and the minimum).
 
-select min(arrival_time - departure_time), max(arrival_time - departure_time) from flights;
+SELECT min(arrival_time - departure_time), max(arrival_time - departure_time) FROM flights;
